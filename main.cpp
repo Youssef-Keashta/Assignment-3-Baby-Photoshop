44c51887f1f6eadbefa3934e999b1f90699678b5
mahmoudHP <157931767+mahmoudHP@users.noreply.github.com>
2024-04-13 06:32:42 -0700

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,32 @@
 
 
 using namespace std;
+// Function to apply the Den Den Mushi filter to the image
+int apply_DenDen_Mushi_Filter(Image& image) {
+    srand(time(NULL));
+
+    // Add static noise
+    for (int y = 0; y < image.height; ++y) {
+        for (int x = 0; x < image.width; ++x) {
+            for (int c = 0; c < image.channels; ++c) {
+                // Add random noise with a certain probability
+                if (rand() % 100 < 5) {
+                    unsigned char noise = rand() % 256;
+                    image.setPixel(x, y, c, noise);
+                }
+            }
+        }
+    }
+    // Ask the user for the output image file name
+    string output_filename;
+    cout << "Please enter the output image name with extension (.jpg, .bmp, .png, .tga): ";
+    cin >> output_filename;
+
+    // Save the inverted image
+    image.saveImage(output_filename);
+    system(output_filename.c_str());
+    return 0 ;
+}
 
 
 // Function to apply blur using 2D prefix sum
@@ -925,10 +951,11 @@ int main() {
             cout << "5. Grayscale Conversion.\n" ;
             cout << "6. Merge Images.\n";
             cout << "7. Blur Image.\n";
-            cout<< "8. Rotate Image.\n";
-            cout<< "9. Resize Image.\n";
-            cout<< "10. Picture Frame.\n";
-            cout<< "11. Sunlight Effect.\n";
+            cout << "8. Rotate Image.\n";
+            cout << "9. Resize Image.\n";
+            cout << "10. Picture Frame.\n";
+            cout << "11. Sunlight Effect.\n";
+            cout << "12. "
             char filter_choice;
             cin >> filter_choice;
             cout << "Please enter the image name: ";
@@ -979,6 +1006,10 @@ int main() {
                 }
                 case '11' :{
                     SunshineEffect(image_name);
+                }
+                 case '12' : {
+                    Image my_image (image_name) ;
+                    apply_DenDen_Mushi_Filter (my_image);
                 }
                 default : {
                     cout << " Invalid Input.";
